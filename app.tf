db13f06e43b88c6158e31f5d2a3646b8658354f5
darshan30899 <85397437+darshan30899@users.noreply.github.com>
2024-03-07 14:54:55 +0530

    Update app.tf

--- app.tf
+++ app.tf
@@ -1,11 +1,20 @@
-provider "github" {
-  organization = "darshan30899"
-  token = "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"
-}
-
-resource "github_repository" "example" {
-  name        = "Newrepo"
-  description = "My awesome codebase"
-
-  visibility = "public"
-}
+ terraform {
+      required_providers {
+        github = {
+          source  = "integrations/github"
+          version = "~> 4.0"
+        }
+      }
+    }
+    
+    provider "github" {
+      token = "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"
+    }
+    
+    resource "github_repository" "terraform" {
+      name        = "Terraform"
+      description = "Test for terraform intergration"
+    
+      visibility = "private"
+    
+    }
