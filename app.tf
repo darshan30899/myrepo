fff849b5036eda59ef1c35c4b5bdba095f7474e2
darshan30899 <85397437+darshan30899@users.noreply.github.com>
2024-03-07 15:01:12 +0530

    Update app.tf

--- app.tf
+++ app.tf
@@ -1,20 +1,9 @@
- terraform {
-      required_providers {
-        github = {
-          source  = "integrations/github"
-          version = "~> 4.0"
-        }
-      }
-    }
+provider "github" {
+    token = "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"
+ }
     
-    provider "github" {
-      token = "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"
-    }
-    
-    resource "github_repository" "terraform" {
-      name        = "Terraform"
-      description = "Test for terraform intergration"
-    
-      visibility = "private"
-    
-    }
+resource "github_repository" "terraform" {
+    name        = "Terraform"
+    description = "Test for terraform intergration"
+    visibility = "private"
+  }
